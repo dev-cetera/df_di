17bd01bc4d6d908e5c35c03c71d00d572a036ebc
robmllze <86869208+robmllze@users.noreply.github.com>
2025-02-26 11:07:55 +1100

    +chore: Update dependencies

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:df_di/df_di.dart';
 
+// Here we test if we can successfully get a registered value using getT when
+// the app is compiled with WASM and minification via "flutte run -d --wasm --release".
 void main() {
-  DI.global.register<int>(42);
-  final answer = DI.global<int>().unwrap();
+  DI.global.register<List<int>>([42]);
+  final answer = DI.global.getT(List<int>).unwrap().unwrap();
+  // Should print 42.
+  print(answer);
   runApp(MaterialApp(home: Scaffold(body: Text('$answer'))));
 }
